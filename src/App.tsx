de901b123203688217be151e842c393f0a468131
Florencia Yáñez <floyanezg@gmail.com>
2025-02-15 16:03:13 -0300

    ✨feature: download json template

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,22 @@ const azul = lightBlue[600];
 const rosado = pink[500];
 const verde = lightGreen[700];
 
+async function handleDownloadTemplate() {
+  const response = await fetch('/template.json');
+  const data = await response.blob();
+
+  // Crea la URL del Blob
+  const url = URL.createObjectURL(data);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'template.json';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
+
+
 function App() {
   return (
     <>
@@ -24,7 +40,7 @@ function App() {
         <Button variant="contained" endIcon={<Upload />} sx={{ m: 1, backgroundColor: rosado }}>
           Load Game
         </Button>
-        <Button variant="contained" endIcon={<Download />} sx={{ m: 1, backgroundColor: verde}}>
+        <Button variant="contained" endIcon={<Download />} sx={{ m: 1, backgroundColor: verde}} onClick={handleDownloadTemplate}>
           Download Template
         </Button>
       </div>
